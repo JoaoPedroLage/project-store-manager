90e2d5cc2f4215a501cbe8d682ad46ecde338b11
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-24 21:25:07 -0300

    feat: creats /sales and /products routes

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const productsControllers = require('../controllers/productsControllers');
+
 const router = express.Router();
+
+router.get(
+  '/',
+  productsControllers.getAll,
+);
+
+router.get(
+  '/:id',
+  productsControllers.getById,
+);
+
+// router.post(
+//   '/',
+//   productsControllers.,
+// );
 
 module.exports = router;
--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -1,5 +1,22 @@
 const express = require('express');
 
+const salesControllers = require('../controllers/salesControllers');
+
 const router = express.Router();
+
+router.get(
+  '/',
+  salesControllers.getAll,
+);
+
+router.get(
+  '/:id',
+  salesControllers.getById,
+);
+
+// router.post(
+//   '/',
+//   productsControllers.,
+// );
 
 module.exports = router;
