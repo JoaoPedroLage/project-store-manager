1e2002bd89e15a136204b6ac397c36b211503380
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-26 16:33:09 -0300

    feat: logic to save in database a new sale in sales and sale_products tables

--- models/salesModels.js
+++ models/salesModels.js
@@ -37,10 +37,31 @@ const getById = async (id) => {
   const [result] = await connection.execute(SQL, [id]);
   
   if (!result.length) return null;
+
+  return result;
+};
+
+const createSale = async () => {
+  const SQL = 'INSERT INTO StoreManager.sales (date) VALUES(NOW());';
+
+  const [result] = await connection.execute(SQL);
+
+  return result;
+};
+
+const salesRecord = async (productId, quantity, id) => {
+  const SQL = (
+    'INSERT StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);'
+    );
+
+  const [result] = await connection.execute(SQL, [id, productId, quantity]);
+
   return result;
 };
 
 module.exports = {
   getAll,
   getById,
+  createSale,
+  salesRecord,
 };
