81a541459fed0b9449687b01dc1a902d9d7c21e3
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-28 12:18:13 -0300

    refactor: wrong validation schema to new sale data

--- schemas/validationCreateSale.js
+++ schemas/validationCreateSale.js
@@ -6,19 +6,19 @@ module.exports = Joi.object({
   .required()
   .integer()
   .messages({
-    'string.base': '422|"productId" must be a number',
+    'number.base': '422|"productId" must be a number',
     'any.required': '400|"productId" is required',
-    'number.integer': '422|"quantity" must an integer',
+    'number.integer': '422|"productId" must an integer',
   }),
   quantity: Joi.number()
   .strict()
   .required()
   .integer()
-  .min(1)
+  .greater(0)
   .messages({
     'number.base': '422|"quantity" must be a number',
     'any.required': '400|"quantity" is required',
     'number.integer': '422|"quantity" must an integer',
-    'number.min': '422|"quantity" must be greater than or equal to 1',
+    'number.greater': '422|"quantity" must be greater than or equal to 1',
   }),
 });
